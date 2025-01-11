5682243e8a36bfa23703c0a67f05b551d33621c3
Md. Injamam Hossain <injamam075@gmail.com>
2025-01-11 08:05:52 +0600

    1400. Construct K Palindrome Strings

--- Construct_K_Palindrome_Strings.cpp
+++ Construct_K_Palindrome_Strings.cpp
@@ -4,22 +4,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution 
+{
 public:
-    bool canConstruct(string s, int k) {
+    bool canConstruct(string s, int k) 
+    {
         // Base condition: If k is greater than the length of the string, it's impossible
         if (k > s.length()) return false;
 
         // Count the frequency of each character
         unordered_map<char, int> freq;
-        for (char c : s) {
+        for (char c : s) 
+        {
             freq[c]++;
         }
 
         // Count the number of characters with odd frequencies
         int oddCount = 0;
-        for (auto it : freq) {
-            if (it.second % 2 != 0) {
+        for (auto it : freq) 
+        {
+            if (it.second % 2 != 0) 
+            {
                 oddCount++;
             }
         }
