05908ff10c2b6ba42733835b808925f047dc5f4a
Md. Injamam Hossain <injamam075@gmail.com>
2025-01-18 17:20:50 +0600

    palindrom_number

--- palindrom_number.cpp
+++ palindrom_number.cpp
@@ -1,21 +1,25 @@
 // 9. palindrome number
 // cpp Solution! problem-9
 
-
-class Solution {
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
 public:
-    bool isPalindrome(int x) {
+    bool isPalindrome(int x)
+    {
         // Negative numbers are not palindromes
-        if (x < 0 || (x % 10 == 0 && x != 0)) 
+        if (x < 0 || (x % 10 == 0 && x != 0))
             return false;
-        
+
         // Reverse the second half of the number
         int reversedHalf = 0;
-        while (x > reversedHalf) {
-            reversedHalf = reversedHalf * 10 + x % 10;  // Append the last digit
-            x /= 10;  // Remove the last digit from x
+        while (x > reversedHalf)
+        {
+            reversedHalf = reversedHalf * 10 + x % 10; // Append the last digit
+            x /= 10;                                   // Remove the last digit from x
         }
-        
+
         // Compare the original half and reversed half
         return (x == reversedHalf || x == reversedHalf / 10); // Handles odd-length cases
     }
