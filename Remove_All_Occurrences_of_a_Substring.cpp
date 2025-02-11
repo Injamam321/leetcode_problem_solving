e622102671f7e0b415c15b81fcd73005ea802173
Md. Injamam Hossain <injamam075@gmail.com>
2025-02-11 23:48:28 +0600

    1910. Remove All Occurrences of a Substring

--- Remove_All_Occurrences_of_a_Substring.cpp
+++ Remove_All_Occurrences_of_a_Substring.cpp
@@ -1,5 +1,32 @@
-// 3160. Find the Number of Distinct Colors Among the Balls
-// cpp Solution! problem-3160
+// 1910. Remove All Occurrences of a Substring
+// cpp Solution! problem-1910
 
 #include <bits/stdc++.h>
 using namespace std;
+
+class Solution
+{
+public:
+    string removeOccurrences(string s, string part)
+    {
+        int partLen = part.size();
+        string result;
+        for (char c : s)
+        {
+            result.push_back(c);
+            if (result.size() >= partLen && result.substr(result.size() - partLen) == part)
+            {
+                result.erase(result.size() - partLen, partLen);
+            }
+        }
+        return result;
+    }
+};
+
+// Complexity:
+
+// Time complexity:
+// O(N²)
+
+// Space complexity:
+// O(N)
