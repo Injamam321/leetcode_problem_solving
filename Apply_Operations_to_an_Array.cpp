92cfe0ea426f70d9577e2f2667c7127cf23c3153
Md. Injamam Hossain <injamam075@gmail.com>
2025-03-07 10:10:42 +0600

    Merge_Two_2D_Arrays_by_Summing_Values.cpp

--- Apply_Operations_to_an_Array.cpp
+++ Apply_Operations_to_an_Array.cpp
@@ -1,2 +1,40 @@
 // 2460. Apply Operations to an Array
 // cpp Solution! problem-2460
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> applyOperations(vector<int> &nums)
+    {
+        int n = nums.size();
+        int i = 0, j = i + 1;
+        while (j <= n - 1 && i <= n)
+        {
+            if (nums[i] == nums[j])
+            {
+                nums[i] *= 2;
+                nums[j] = 0;
+            }
+
+            i++;
+            j++;
+        }
+        int notZero = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[notZero] = nums[i];
+                if (notZero != i)
+                {
+                    nums[i] = 0;
+                }
+                notZero++;
+            }
+        }
+        return nums;
+    }
+};
